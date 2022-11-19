08c84f73d8d196fad11cacc6c78884ec157de7d0
lytosdev <sanchezmartincm@gmail.com>
2022-11-19 17:17:04 +0100

    Primer commit

---
+++ src/templates/RecetaVistaPreviaController.java
@@ -0,0 +1,21 @@
+package templates;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+
+public class RecetaVistaPreviaController implements Initializable {
+
+    public RecetaVistaPreviaController() {
+
+    }
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+
+    }
+
+}
