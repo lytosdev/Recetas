f2f21ec9035184933f89afdb01328faf18d929ba
lytosdev <sanchezmartincm@gmail.com>
2022-11-23 23:46:31 +0100

    Crea y modifica vistas

---
+++ src/controlador/VerUtensilioController.java
@@ -0,0 +1,19 @@
+package controlador;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class VerUtensilioController implements Initializable {
+
+    public VerUtensilioController() {
+
+    }
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+
+    }
+
+}
